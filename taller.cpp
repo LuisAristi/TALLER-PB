944e4a89ddb2124083d00b392a36b3687b240745
LuisAristi <santiaristi2015@gmail.com>
2023-10-03 20:19:35 -0500

    lee comentarios

--- taller.cpp
+++ taller.cpp
@@ -9,52 +9,84 @@
 
 using namespace std;
 
-//prototipos de funcion
-int leer();
-bool es_primo(int x, int y, int divisores);
+//prototipos de funcion primos gemelos
+int leer(int &numero0, int &numero1);
+int gemelos(int a, int b);
+bool es_primo(int numero, int divisor);
+
+//prototipos de funcion primos malvados
 
 
-void gemelos(int a, int b);
+
 void clear();
 
 int main() {
     //variables locales
-    int opc;
+    int opc, numero0, numero1, size;
+    char binarios[64];
 
     //comienza el codigo || Dios libranos del espagueti :D
-    cout<<"ingrese una de las siguientes opciones: "<<endl;
-    cout<<"1. primos gemelos: "<<endl;
-    cout<<"2. primos malvados: "<<endl;
-    cout<<"3. aritmetica de punteros: "<<endl;
-    cout<<"4. salir"<<endl;
-    cin>>opc; clear();
+    for(;;){
+        inicio:
+        clear();
+        cout<<"ingrese una de las siguientes opciones: "<<endl;
+        cout<<"1. primos gemelos: "<<endl;
+        cout<<"2. primos malvados: "<<endl;
+        cout<<"3. aritmetica de punteros: "<<endl;
+        cout<<"4. salir"<<endl;
+        cin>>opc; clear();
     
-    switch(opc){
-        case 1:
-            
-            break;
+        switch(opc){
+            case 1:
+                while(opc == 1){
+                    clear();
+                    leer(numero0, numero1);
+                    if(gemelos(numero0, numero1) == 2){
+                        cout<<endl<<"son numeros primos gemelos!!";
+                    }
+                    else cout<<endl<<"no son numeros primos gemelos :()";
+                    cout<<endl<<endl<<"------ presione cualquier numero distinto de 1 para salir o 1 para repetir: ";
+                    cin>>opc;
+                }
+                goto inicio;
+            case 2:
+                while(opc == 1){
+                    clear();
+                    
+                    
+
+                    cout<<endl<<endl<<"------ presione cualquier numero distinto de 1 para salir o 1 para repetir: ";
+                    cin>>opc;
+                }
+                break;
+        }
+
+        return 0;
+    }
+}
+
+//funciones
+int leer(int &numero0, int &numero1){
+    label1: //goto es literalmente ir a, cuando decimos goto label1; regresamos aca, si usamos break eb vez de goto sale error en el compilador.
+    clear();
+    cout<<"ingrese el primer numero: "; cin>>numero0;
+
+    if(numero0 <= 1 || es_primo(numero0, 2) == false ){ 
+        cout<<"recuerde ingresar un numero primo"<<endl; sleep(1); goto label1;
+    }
+
+    cout<<endl<<"ingrese el segundo numero: "; cin>>numero1;
+
+    if(numero1 <= 1 || es_primo(numero1, 2) == false ){ 
+        cout<<"recuerde ingresar un numero primo"<<endl; sleep(1); goto label1;
     }
 
     return 0;
 }
 
-//funciones
-int leer(){
-    int numero0, numero1;
-    cout<<"ingrese el primer numero: "<<endl; cin>>numero0;
-
-    if(numero0 <= 1){ 
-        cout<<"recuerde ingresar un numero primo"<<endl; sleep(1); return 0;
-    }
-
-    cout<<"ingrese el segundo numero: "<<endl; cin>>numero1;
-
-    if(numero1 <= 1){ 
-        cout<<"recuerde ingresar un numero primo"<<endl; sleep(1); return 0;
-    }
-
-    return 0;
-
+int gemelos(int a, int b){
+    b -= a;
+    return b;
 }
 
 bool es_primo(int numero, int divisor = 2){
@@ -70,13 +102,7 @@ bool es_primo(int numero, int divisor = 2){
 
 
 //metodos
-void gemelos(int a, int b){
-    b -= a;
-    if(b == 2){
-        cout<<"son primos gemelos!!"<<endl;
-    }
-    else cout<<"no son primos gemelos"<<endl;
-}
+
 
 //hay que agilizar tiempo y hay gente que usa linux || platform tolerant is thhe way
 void clear(){
